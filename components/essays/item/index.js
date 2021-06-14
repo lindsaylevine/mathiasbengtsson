7a74eb50f487c6c221d56160a1a00d977e3cdf03
mathiasdag <mathias@asso.gd>
2021-06-14 16:03:59 +0200

    ...

--- components/essays/item/index.js
+++ components/essays/item/index.js
@@ -3,8 +3,11 @@ import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import styles from './item.module.css'
 import { objectToInlineList, formatDate } from '../../../lib/utils'
 import Reference from './reference'
+import { useState } from 'react'
+import { useSwipeable } from 'react-swipeable'
 
 export default function Item({ data, index }) {
+  const [showRef, setShowRef] = useState(false)
   const {
     authors,
     date,
@@ -13,6 +16,15 @@ export default function Item({ data, index }) {
     source,
     slug
   } = data
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => setShowRef(true),
+    onSwipedRight: () => setShowRef(false),
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true
+  })
+
+  const Toggle = ({ left, right }) => <div className={styles.toggle} style={left ? { left: 0 } : { right: 0 }}  onClick={() => setShowRef(!showRef)} />
 
   const figures = article.content.filter(k => k.content.some(f => f.nodeType === "embedded-entry-inline"))
 
@@ -37,11 +49,11 @@ export default function Item({ data, index }) {
 
   return (
     <article className={styles.container + " soft-breaks block"}>
-      <header className={"col-2"}>
+      <header className={"col-1-2"}>
         <div>{objectToInlineList(authors)}:</div>
         <h1 style={{ margin: 0 }}>–{title}–</h1>
       </header>
-      <section className={styles.body + " col-2"}>
+      <section className={`${styles.body} col-1-2 ${showRef ? styles.refView : styles.bodyView}`} {...handlers}>
         <div>
           {formatDate(date)}<br />
           {source}
@@ -50,6 +62,8 @@ export default function Item({ data, index }) {
           {documentToReactComponents(article, options)}
         </div>
       </section>
+      <Toggle left />
+      <Toggle right />
     </article>
   )
 }

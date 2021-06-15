c247b4fb9afa315baf3a9d66ce989af5fd33554d
mathiasdag <mathias@asso.gd>
2021-06-15 10:09:04 +0200

    ...

--- components/biography/index.js
+++ components/biography/index.js
@@ -3,7 +3,7 @@ import Entry from './entry'
 export default function Biography({ data }) {
 
   return (
-    <section className="block col-2 soft-breaks">
+    <section className="block col-1-2 soft-breaks">
       {data.components.slice(0, 2).map((entry, i) => (
         <Entry data={entry} />
       ))}
